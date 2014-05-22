fe77fb209284c733a13cd2c96302fd80887a7ad5
Rahul Ranjan <rahul.rrixe@gmail.com>
2014-05-23 03:20:07 +0530

    initial commits

---
+++ setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+PROJECT = 'libcloudCLI'
+
+# Change docs/sphinx/conf.py too!
+VERSION = '0.1'
+
+from setuptools import setup, find_packages
+
+try:
+    long_description = open('README.md', 'rt').read()
+except IOError:
+    long_description = ''
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+
+    description='Licloud CLI App',
+    long_description=long_description,
+
+    author='Tomaz Muraus, Rahul Ranjan',
+    author_email='rahul.rrixe@gmail.com',
+
+    url='https://github.com/rahulrrixe/libcloudCLI',
+    download_url='https://github.com/rahulrrixe/libcloudCLI/tarball/master',
+
+    classifiers=['Development Status :: 3 - Alpha',
+                 'License :: OSI Approved :: Apache Software License',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.2',
+                 'Intended Audience :: Developers',
+                 'Environment :: Console',
+                 ],
+
+    platforms=['Any'],
+
+    scripts=[],
+
+    provides=[],
+    install_requires=['cliff', 'libcloud_rest'],
+
+    namespace_packages=[],
+    packages=find_packages(),
+    include_package_data=True,
+
+    entry_points={
+        'console_scripts': [
+            'libcloudcli = libcloudcli.main:main'
+        ],
+        'cliff.demo': [
+            'commands = libcloudcli.commands:Commands',
+            'error = cliffdemo.simple:Error',
+            'unicode = cliffdemo.encoding:Encoding',
+        ],
+    },
+
+    zip_safe=False,
+)
