7efb8ac2475ce587fcef0d715ac6ebe953a621a5
Rahul Ranjan <rahul.rrixe@gmail.com>
2014-06-11 23:17:28 -0400

    initial commit

---
+++ libcloudcli/configure.py
@@ -0,0 +1,19 @@
+import os
+import re
+import sys 
+import logging
+#import configparser
+
+#config = configparser.ConfigParser()
+
+# Find the path of the config file
+path = os.path
+parent_dir = os.pardir
+
+for path in parent_dir:
+    print path 
+
+
+
+
+
--- libcloudcli/main.py
+++ libcloudcli/main.py
@@ -1,21 +1,48 @@
+import argparse
+import getpass
 import logging
+import os
 import sys
+import traceback
 
 from cliff.app import App
+from cliff import command
+from cliff import complete
+from cliff import help
 from commands import AWS, File, Files, Compute, CreateNode
 from cliff.commandmanager import CommandManager
 
 
 class LibcloudCLI(App):
+
+    CONSOLE_MESSAGE_FORMAT = '%(levelname)s: %(name)s %(message)s'
+
     log = logging.getLogger(__name__)
 
     def __init__(self):
-        command = CommandManager('libcloudcli.app')
+        # Patch command.Command to add a default auth_required = True
+        command.Command.auth_required = True
+        command.Command.best_effort = False
+        # but for help there should not be any authorisation
+        help.HelpCommand.auth_required = False
+        complete.CompleteCommand.best_effort = True
+
+        ex_command = CommandManager('libcloud.cli')
         super(LibcloudCLI, self).__init__(
             description='Libcloud CLI App',
             version='0.1',
-            command_manager=command,
-        )
+            command_manager=ex_command,)
+
+        # Show stack traces
+        self.dump_stack_trace = True
+
+        # This is instantiated in initialize_app() only when using
+        # password flow auth
+        self.auth_client = None
+
+        # Assume TLS host certificate verification is enabled
+        self.verify = True
+
         commands = {
             'aws': AWS,
             'file': File,
@@ -24,7 +51,7 @@ class LibcloudCLI(App):
             'createnode': CreateNode
         }
         for k, v in commands.iteritems():
-            command.add_command(k, v)
+            ex_command.add_command(k, v)
 
     def initialize_app(self, argv):
         self.log.debug('initialize_app')
