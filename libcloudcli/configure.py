ea9216781592bf77e0a0b18cc8330b65d1e7fb0c
Rahul Ranjan <rahul.rrixe@gmail.com>
2014-06-13 00:19:18 -0400

    add configparser

--- libcloudcli/configure.py
+++ libcloudcli/configure.py
@@ -1,19 +1,24 @@
 import os
 import re
-import sys 
+import sys
 import logging
-#import configparser
-
-#config = configparser.ConfigParser()
-
-# Find the path of the config file
-path = os.path
-parent_dir = os.pardir
-
-for path in parent_dir:
-    print path 
+import configparser
 
 
+def main():
+    try:
+        root = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        # currently config.ini is in example folder
+        config_path = os.path.join(root, 'examples/config.ini')
+        # TODO: There is a better way to log this message than print.
+        print "Reading secrets from %r" % secret_path
 
+    parser = configparser.ConfigParser()
+        parser.read(config_path)
+        get_config = dict(parser.items("default"))
+    except Exception as e:
+        # TODO: There is a better way to log this message than print.
+        print 'Failed to load config.ini.  Reason: %r' % str(e)
 
-
+if __name__ == '__main__':
+    main()
