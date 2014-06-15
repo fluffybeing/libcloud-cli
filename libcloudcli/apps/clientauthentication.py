8a847858a00ca0cef6930d3c10b154c551d1ff37
Rahul Ranjan <rahul.rrixe@gmail.com>
2014-06-15 19:35:09 -0400

    add cache method

--- libcloudcli/apps/clientauthentication.py
+++ libcloudcli/apps/clientauthentication.py
@@ -6,4 +6,15 @@ import sys
 
 LOG = logging.getLogger(__name__)
 
-class ClientCache
+class ClientCache(object):
+    """ Descripter class for caching created client"""
+    def __init__(self, factory):
+        self.factory = factory
+        self._handle = None
+
+    def __get__(self, instance, owner):
+        # Tell the ClientManager to login to keystone
+        if self._handle is None:
+            self._handle = self.factory(instance)
+        return self._handle
+
