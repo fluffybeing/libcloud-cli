f166b24a1653fb834c063ce45877ad0c6a56bef5
Rahul Ranjan <rahul.rrixe@gmail.com>
2014-06-26 06:40:08 -0400

    interface class

---
+++ libcloudcli/apps/interface.py
@@ -0,0 +1,28 @@
+# interface to link up the methods info and the dynamic class builder
+
+from provider import  DriverMethod, get_providers_info
+
+
+
+# get list of the supported provider
+def  providerList():
+    result = get_providers_info()
+    print result
+
+# get list of methods supported by provider
+def provderSupportedMethod():
+    pass
+
+# create the instance
+def driver_instance():
+    pass
+
+# request dynamic builder to create the command class
+def buildCommandClass():
+    pass
+
+# get method info
+def methodInfo()
+    pass
+
+
